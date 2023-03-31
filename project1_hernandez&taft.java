66c5dac78d3edf25fd8bdb578bfff7cf6b3893f8
Gio <giovanniahernandez20@gmail.com>
2023-03-31 10:50:57 -0400

    modified:   project1_hernandez&taft.java

--- project1_hernandez&taft.java
+++ project1_hernandez&taft.java
@@ -1,15 +1,24 @@
 import java.util.concurrent.*;
+import java.util.Random;
 import java.util.Deque;
 import java.util.ArrayDeque;
 
 class SynchedBridge {
 
     public static void main(String[] args) {
-        
+
     }
     
     static class Eastbound implements Runnable {
         protected Deque<Integer> eastboundCars;
+
+        public Eastbound() {
+            eastboundCars = new ArrayDeque<>();
+        }
+
+        public void createRandomCars() {
+
+        }
 
         @Override
         public void run() {
@@ -20,6 +29,10 @@ class SynchedBridge {
 
     static class Westbound implements Runnable {
         protected Deque<Integer> westboundCars;
+
+        public Westbound() {
+            westboundCars = new ArrayDeque<>();
+        }
 
         @Override
         public void run() {
