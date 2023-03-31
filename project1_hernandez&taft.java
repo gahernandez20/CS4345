4b7c8aa8d6ab3cc02f58f5ba04299861d00d60d6
Gio <giovanniahernandez20@gmail.com>
2023-03-31 10:40:36 -0400

    modified:   project1_hernandez&taft.java

--- project1_hernandez&taft.java
+++ project1_hernandez&taft.java
@@ -1,10 +1,30 @@
+import java.util.concurrent.*;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 class SynchedBridge {
+
+    public static void main(String[] args) {
+        
+    }
     
     static class Eastbound implements Runnable {
+        protected Deque<Integer> eastboundCars;
+
+        @Override
+        public void run() {
+            throw new UnsupportedOperationException("Unimplemented method 'run'");
+        }
 
     }
 
     static class Westbound implements Runnable {
+        protected Deque<Integer> westboundCars;
+
+        @Override
+        public void run() {
+            throw new UnsupportedOperationException("Unimplemented method 'run'");
+        }
 
     }
 }
