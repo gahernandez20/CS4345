a98675b8777a83f6d41ac327403bad076bb78f51
Gio <giovanniahernandez20@gmail.com>
2023-04-01 20:20:22 -0400

    modified:   SynchedBridge$Eastbound.class 	modified:   SynchedBridge$Westbound.class 	modified:   SynchedBridge.class 	modified:   project1_hernandez&taft.java

--- project1_hernandez&taft.java
+++ project1_hernandez&taft.java
@@ -12,11 +12,11 @@ class SynchedBridge {
 
     public static void main(String[] args) {
         Thread eastBoundThread = new Thread(new Eastbound());
-        Thread westBoundThread = new Thread(new Westbound());
-        //eastbound.createRandomCars();
-        
+        // Thread westBoundThread = new Thread(new Westbound());
+        // eastbound.createRandomCars();
+        eastBoundThread.start();
     }
-    
+
     static class Eastbound implements Runnable {
         protected Deque<Integer> eastboundCars;
 
@@ -26,19 +26,27 @@ class SynchedBridge {
 
         public void createRandomCars() {
             Random rand = new Random();
-            for(int i=0; i<10; i++) {
-                int car = rand.nextInt(1,10000);
-                while(car % 2 == 0) {
-                    car = rand.nextInt(1,10000);
+            
+            while(true){
+                int car = rand.nextInt(1, 10000);
+                while (car % 2 == 0) {
+                    car = rand.nextInt(1, 10000);
                 }
                 eastboundCars.addLast(car);
-            }
+                try {
+                    Thread.sleep(2000);
+                } 
+                catch(InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+                System.out.printf("Car %d is passing.\n", car);
+            }            
         }
 
         public Deque<Integer> getCars() {
             return eastboundCars;
         }
-
+        
         @Override
         public void run() {
             createRandomCars();
@@ -55,13 +63,20 @@ class SynchedBridge {
 
         public void createRandomCars() {
             Random rand = new Random();
-            for(int i=0; i<10; i++) {
-                int car = rand.nextInt(1,10000);
-                while(car % 2 == 0) {
-                    car = rand.nextInt(1,10000);
+            while(true){
+                int car = rand.nextInt(1, 10000);
+                while (car % 2 != 0) {
+                    car = rand.nextInt(1, 10000);
                 }
                 westboundCars.addLast(car);
-            }
+                try {
+                    Thread.sleep(2000);
+                } 
+                catch(InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+                System.out.printf("Car %d is passing.\n", car);
+            }  
         }
 
         @Override
